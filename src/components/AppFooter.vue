96801e0c11cdd008ea721ea7577551ac1c68c84a
Enrique Mireles <enrique.mireles@udem.edu>
2020-03-23 12:04:19 -0600

    Add social links to share in the footer.

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,22 +1,35 @@
 <template>
-  <v-footer color="grey lighten-3 py-2 px-4">
-    <v-spacer></v-spacer>
-    <div>
-      {{ new Date().getFullYear() }}
-      —
-      Hecho con
-      <span
-        class="red--text"
-      >&#9829;</span>
-      por
-      <a href="https://github.com/emirelesg">@emirelesg</a>
-    </div>
+  <v-footer padless>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-card flat tile color="grey lighten-4" class="text-center">
+          <v-card-text>
+            <div class="caption">Compártelo en tus redes</div>
+            <share/>
+            <div class="mt-3">
+              {{ new Date().getFullYear() }}
+              —
+              Hecho con
+              <span
+                class="red--text"
+              >&#9829;</span>
+              por
+              <a href="https://github.com/emirelesg">@emirelesg</a>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
 <script>
+import Share from '@/components/Share.vue';
 export default {
   name: 'AppFooter',
+  components: {
+    Share
+  },
   data() {
     return {};
   }
---
+++ src/components/Share.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-btn
+      class="mx-2"
+      @click="share(network.url)"
+      icon
+      color="primary"
+      v-for="network in networks"
+      :key="network.icon"
+    >
+      <v-icon>{{ network.icon }}</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Share',
+  data() {
+    return {
+      url: 'https://covid19.newtondreams.com/',
+      title: 'COVID-19 en México',
+      networks: [
+        {
+          icon: 'mdi-facebook',
+          url: 'https://www.facebook.com/sharer/sharer.php?u=@url&title=@title'
+        },
+        {
+          icon: 'mdi-twitter',
+          url: 'https://twitter.com/intent/tweet?text=@title&url=@url'
+        },
+        {
+          icon: 'mdi-linkedin',
+          url:
+            'https://www.linkedin.com/shareArticle?mini=true&url=@url&title=@title'
+        }
+      ]
+    };
+  },
+  methods: {
+    share(url) {
+      const formatted = url
+        .replace(/@url/g, encodeURIComponent(this.url))
+        .replace(/@title/g, encodeURIComponent(this.title));
+      const popupWindow = window.open(
+        formatted,
+        '_blank',
+        'status=no' + 'scrollbars=no'
+      );
+      popupWindow.focus();
+    }
+  }
+};
+</script>
