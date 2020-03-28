36f155475fc61f78480246ead6e90aa55adf5f8d
Enrique Mireles <enrique.mireles@udem.edu>
2020-03-28 14:39:34 -0600

    configure webpack to load meta tags and title.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
-  productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'COVID-19 en México';
+      args[0].meta = {
+        'twitter:card': 'summary',
+        'twitter:creator': '@emirelesg',
+        'og:title': `${args[0].title}`,
+        'og:type': 'product',
+        'og:url': 'https://covid19.newtondreams.com/',
+        'og:description':
+          'Herramienta para ver la situación actual del COVID-19 en México con gráficas interactivas.'
+      };
+      return args;
+    });
+  }
 };
