8041adc5be3ec69585b7a8db08e70d20e7ea83d1
Enrique Mireles <enrique.mireles@udem.edu>
2020-03-29 17:36:14 -0600

    display the change in confirmed cases

--- src/components/tables/CasesByStateTable.vue
+++ src/components/tables/CasesByStateTable.vue
@@ -14,17 +14,32 @@
             disable-pagination
             dense
             height="400px"
-            sort-by="name"
+            sort-by="confirmed"
+            :sort-desc="true"
             :headers="headers"
             :items="data"
             item-key="name"
           >
+            <template v-slot:item.confirmedDelta="{ item }">
+              <span
+                v-if="item.confirmedDelta !== 0"
+                :style="getDeltaStyle(item.confirmedDelta)"
+                >{{ getDeltaLabel(item.confirmedDelta) }}</span
+              >
+            </template>
             <template v-slot:body.append>
               <tr v-show="$vuetify.breakpoint.smAndUp">
                 <td class="font-weight-bold caption">Total</td>
-                <td class="font-weight-bold">{{ stats.confirmed }}</td>
-                <td class="font-weight-bold">{{ stats.suspected }}</td>
-                <td class="font-weight-bold">{{ stats.deaths }}</td>
+                <td class="text-center font-weight-bold">
+                  {{ stats.confirmed }}
+                </td>
+                <td class="text-center font-weight-bold">
+                  {{ getDeltaLabel(stats.confirmedDelta) }}
+                </td>
+                <td class="text-center font-weight-bold">
+                  {{ stats.suspected }}
+                </td>
+                <td class="text-center font-weight-bold">{{ stats.deaths }}</td>
               </tr>
             </template>
           </v-data-table>
@@ -35,6 +50,7 @@
 </template>
 
 <script>
+import colors from 'vuetify/lib/util/colors';
 import Loading from '@/components/Loading.vue';
 import { mapState } from 'vuex';
 export default {
@@ -46,11 +62,30 @@ export default {
     return {
       headers: [
         { text: 'Entidad', value: 'name' },
-        { text: 'Confirmados', value: 'confirmed' },
-        { text: 'Sospechosos', value: 'suspected' },
-        { text: 'Fallecidos', value: 'deaths' }
+        { text: 'Confirmados', value: 'confirmed', align: 'center' },
+        {
+          text: 'Nuevos Confirmados',
+          value: 'confirmedDelta',
+          align: 'center'
+        },
+        { text: 'Sospechosos', value: 'suspected', align: 'center' },
+        { text: 'Fallecidos', value: 'deaths', align: 'center' }
       ]
     };
+  },
+  methods: {
+    getDeltaLabel(val) {
+      if (val > 0) return `+${val}`;
+      return val;
+    },
+    getDeltaStyle(val) {
+      return {
+        padding: '0rem 0.75rem',
+        borderRadius: '5px',
+        backgroundColor:
+          val > 0 ? colors.orange.lighten3 : colors.green.lighten3
+      };
+    }
   },
   computed: {
     ...mapState({
