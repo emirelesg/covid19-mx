1f7a1c46bacb2041702c96f6e64cf1f6c946c652
Enrique Mireles <enrique.mireles@udem.edu>
2020-04-23 14:24:44 -0500

    updated wording

--- src/components/Donations.vue
+++ src/components/Donations.vue
@@ -1,7 +1,7 @@
 <template>
   <card
     title="Apoya el Proyecto"
-    subtitle="Realiza una pequeña donación"
+    subtitle="Esto no sería posible sin tu ayuda"
     :loaded="true"
   >
     <template v-slot:content>
@@ -10,8 +10,11 @@
         <a target="_blank" :href="licenseUrl">licencia MIT</a>. Es y será un
         proyecto gratuito para su uso. Sin embargo, el tiempo y la dedicación
         que toma mantener el sitio actualizado y seguir desarrollando
-        funcionalidades no es sostenible sin apoyo económico. Puedes apoyar el
-        proyecto haciendo una donación única a través de PayPal:
+        funcionalidades no es sostenible sin apoyo económico.
+      </p>
+      <p>
+        Tu puedes apoyar el proyecto haciendo una pequeña donación única a
+        través de PayPal (clic en la imagen):
       </p>
       <v-row align="center" justify="center" class="mb-2">
         <a href="https://www.paypal.me/emirelesg" target="_blank">
